0b8290daf6ffcf2d5aa4adec3678b77a77dfa381
sameed-gilani <sameed.gilani@dubizzlelabs.com>
2023-10-24 10:21:27 +0500

    Finished calling all the seperate componenets

--- hooks_practice/src/App.js
+++ hooks_practice/src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
 import UsingRef from "./useRef";
+import UsingMemo from "./useMemo";
+import UsingCallback from "./useCallback";
 
 function App() {
   return (
     <div>
 
+        <h1>UseRef: </h1>
       <UsingRef/>
+
+        <h1>UseMemo: </h1>
+        <UsingMemo/>
+
+        <h1>UseCallback: </h1>
+        <UsingCallback/>
 
     </div>
   );
