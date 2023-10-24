f89743d7246932414b3b8b903a12e63905271983
sameed-gilani <sameed.gilani@dubizzlelabs.com>
2023-10-24 11:57:46 +0500

    started useReducer stuff

--- hooks_practice/src/App.js
+++ hooks_practice/src/App.js
@@ -2,19 +2,22 @@ import './App.css';
 import UsingRef from "./useRef";
 import UsingMemo from "./useMemo";
 import UsingCallback from "./useCallback";
+import UsingReducer from "./useReducer";
 
 function App() {
   return (
     <div>
 
-        <h1>UseRef: </h1>
-      <UsingRef/>
+      {/*  <h1>UseRef: </h1>*/}
+      {/*<UsingRef/>*/}
 
-        <h1>UseMemo: </h1>
-        <UsingMemo/>
+      {/*  <h1>UseMemo: </h1>*/}
+      {/*  <UsingMemo/>*/}
 
-        <h1>UseCallback: </h1>
-        <UsingCallback/>
+      {/*  <h1>UseCallback: </h1>*/}
+      {/*  <UsingCallback/>*/}
+
+        <UsingReducer/>
 
     </div>
   );
